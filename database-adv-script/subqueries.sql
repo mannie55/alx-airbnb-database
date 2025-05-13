6f02499401ea02d7adbfad92a058e7f5c2c4d330
nnamdi <mannnie55@gmail.com>
2025-05-13 16:08:56 +0100

    fix

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,9 +1,9 @@
-select properties.name, reviews.rating from properties
-left join reviews
-on properties.property_id = reviews.property_id
-where  (select avg(rating) from reviews) > 4.0;
+SELECT properties.name, reviews.rating FROM properties
+LEFT JOIN reviews
+ON properties.property_id = reviews.property_id
+WHERE  (SELECT AVG(rating) FROM reviews) > 4.0;
 
-select users.first_name, users.last_name, count(bookings.user_id) as booking_count
-from users
-inner join bookings on users.user_id = bookings.user_id
-group by users.user_id;
+SELECT users.first_name, users.last_name, COUNT(bookings.user_id) AS booking_count
+FROM users
+INNER JOIN bookings ON users.user_id = bookings.user_id
+GROUP BY users.user_id;
